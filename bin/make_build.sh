e0b9e4038fecfc713c01eee4e252fa32ad0bc993
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-10-19 12:38:04 +0800

    try 1

--- bin/make_build.sh
+++ bin/make_build.sh
@@ -3,6 +3,8 @@
 COMMIT_HASH=$(git rev-parse --short HEAD)
 BUILD_VERSION=$(python build.py version)
 MAJOR_VERSION=$(python build.py version -m)
+NPM_USER=$(npm whoami)
+
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     VERSION=${BUILD_VERSION}
@@ -18,102 +20,108 @@ elif [[ "${TRAVIS_BRANCH}" =~ ^RC-([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
 else
     VERSION=${TRAVIS_BRANCH}
 fi
+NPM_VERSION_INFO=$(npm view anychart@${VERSION})
+
 
 echo Major: ${MAJOR_VERSION}
 echo Version: ${VERSION}
 echo Branch: ${TRAVIS_BRANCH}
 echo Commit Hash: ${COMMIT_HASH}
+echo NPM User: ${NPM_USER}
+echo NPM Info: ${NPM_VERSION_INFO}
 
-# we can build release files only in case of dev release
-if [ "${TRAVIS_BRANCH}" != "master" ]; then
-    # build release files
-    echo Building release files: JS, CSS, Themes
-    python ./build.py compile --output ./dist/js
-    python ./build.py css --output ./dist/css
-    python ./build.py themes --output ./dist/themes
-else
-    echo Skip release files build step, take files from the dist folder as is
-fi
 
-# copy bin files
-echo Copying wrappers from bin to dist
-cp ./bin/binaries_wrapper_end.txt ./dist/binaries_wrapper_end.txt
-cp ./bin/binaries_wrapper_start.txt ./dist/binaries_wrapper_start.txt
 
-# go to dist directory
-cd ./dist/
-
-# don't download docs/demos and remove geodata for dev builds to reduce folder size
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    echo Downloading docs and demos
-
-    # download docs
-    wget https://docs.anychart.com/download -O docs.zip
-    unzip -q docs.zip -d docs
-    rm docs.zip
-
-    # download demos
-    wget https://playground.anychart.com/gallery/latest/download -O demos.zip
-    unzip -q demos.zip -d demos
-    rm demos.zip
-else
-    rm -rf geodata
-fi
-
-# zip files
-zip -q -r installation-package.zip *
-
-# ensure release paths exists and clean
-# as far as cdn always serve removed content, we are free to clean entire folder
-ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
-mkdir -p /apps/static/cdn/releases/${VERSION} &&
-rm -rf /apps/static/cdn/releases/${VERSION}/*"
-
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    ssh -i ~/.ssh/id_rsa  $STATIC_HOST_SSH_STRING "
-    rm -rf /apps/static/cdn/releases/${MAJOR_VERSION}.x.x &&
-    cp -r /apps/static/cdn/releases/${VERSION} /apps/static/cdn/releases/${MAJOR_VERSION}.x.x"
-fi
-
-# upload content
-scp -i ~/.ssh/id_rsa installation-package.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/${VERSION}/anychart-installation-package-${VERSION}.zip
-
-# copy unzip release files and copy to latest
-ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "unzip -q -o /apps/static/cdn/releases/${VERSION}/anychart-installation-package-${VERSION}.zip -d /apps/static/cdn/releases/${VERSION}/"
-
-# copy legacy files by version
-ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
-rm -rf /apps/static/cdn/js/${VERSION} &&
-cp -r /apps/static/cdn/releases/${VERSION}/js /apps/static/cdn/js/${VERSION} &&
-rm -rf /apps/static/cdn/css/${VERSION} &&
-cp -r /apps/static/cdn/releases/${VERSION}/css /apps/static/cdn/css/${VERSION} &&
-rm -rf /apps/static/cdn/themes/${VERSION} &&
-cp -r /apps/static/cdn/releases/${VERSION}/themes /apps/static/cdn/themes/${VERSION} &&
-mkdir -p /apps/static/cdn/schemas/${VERSION} &&
-cp /apps/static/cdn/releases/${VERSION}/json-schema.json /apps/static/cdn/schemas/${VERSION}/json-schema.json &&
-cp /apps/static/cdn/releases/${VERSION}/xml-schema.xsd /apps/static/cdn/schemas/${VERSION}/xml-schema.xsd"
-
-# copy DEV legacy files by version
-if [ "${TRAVIS_BRANCH}" != "master" ]; then
-    ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
-    rm -rf /apps/static/js/${VERSION} &&
-    cp -r /apps/static/cdn/releases/${VERSION}/js /apps/static/js/${VERSION} &&
-    cp /apps/static/cdn/releases/${VERSION}/commit-hash.txt /apps/static/js/${VERSION}/commit-hash.txt &&
-    rm -rf /apps/static/css/${VERSION} &&
-    cp -r /apps/static/cdn/releases/${VERSION}/css /apps/static/css/${VERSION}"
-fi
-
-# copy CAT legacy files by branch name
-if [ "${VERSION}" != "${TRAVIS_BRANCH}" ]; then
-    ssh -i ~/.ssh/id_rsa  $STATIC_HOST_SSH_STRING "
-    rm -rf /apps/static/cdn/releases/${TRAVIS_BRANCH} &&
-    cp -r /apps/static/cdn/releases/${VERSION} /apps/static/cdn/releases/${TRAVIS_BRANCH}"
-fi
-
-# drop cdn cache for uploaded files
-echo Dropping CDN cache
-cd ../
-python ./bin/drop_cdn_cache.py ${VERSION} ${CDN_ALIASE} ${CDN_CONSUMER_KEY} ${CDN_CONSUMER_SECRET} ${CDN_ZONE_ID}
+## we can build release files only in case of dev release
+#if [ "${TRAVIS_BRANCH}" != "master" ]; then
+#    # build release files
+#    echo Building release files: JS, CSS, Themes
+#    python ./build.py compile --output ./dist/js
+#    python ./build.py css --output ./dist/css
+#    python ./build.py themes --output ./dist/themes
+#else
+#    echo Skip release files build step, take files from the dist folder as is
+#fi
+#
+## copy bin files
+#echo Copying wrappers from bin to dist
+#cp ./bin/binaries_wrapper_end.txt ./dist/binaries_wrapper_end.txt
+#cp ./bin/binaries_wrapper_start.txt ./dist/binaries_wrapper_start.txt
+#
+## go to dist directory
+#cd ./dist/
+#
+## don't download docs/demos and remove geodata for dev builds to reduce folder size
+#if [ "${TRAVIS_BRANCH}" = "master" ]; then
+#    echo Downloading docs and demos
+#
+#    # download docs
+#    wget https://docs.anychart.com/download -O docs.zip
+#    unzip -q docs.zip -d docs
+#    rm docs.zip
+#
+#    # download demos
+#    wget https://playground.anychart.com/gallery/latest/download -O demos.zip
+#    unzip -q demos.zip -d demos
+#    rm demos.zip
+#else
+#    rm -rf geodata
+#fi
+#
+## zip files
+#zip -q -r installation-package.zip *
+#
+## ensure release paths exists and clean
+## as far as cdn always serve removed content, we are free to clean entire folder
+#ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
+#mkdir -p /apps/static/cdn/releases/${VERSION} &&
+#rm -rf /apps/static/cdn/releases/${VERSION}/*"
+#
+#if [ "${TRAVIS_BRANCH}" = "master" ]; then
+#    ssh -i ~/.ssh/id_rsa  $STATIC_HOST_SSH_STRING "
+#    rm -rf /apps/static/cdn/releases/${MAJOR_VERSION}.x.x &&
+#    cp -r /apps/static/cdn/releases/${VERSION} /apps/static/cdn/releases/${MAJOR_VERSION}.x.x"
+#fi
+#
+## upload content
+#scp -i ~/.ssh/id_rsa installation-package.zip $STATIC_HOST_SSH_STRING:/apps/static/cdn/releases/${VERSION}/anychart-installation-package-${VERSION}.zip
+#
+## copy unzip release files and copy to latest
+#ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "unzip -q -o /apps/static/cdn/releases/${VERSION}/anychart-installation-package-${VERSION}.zip -d /apps/static/cdn/releases/${VERSION}/"
+#
+## copy legacy files by version
+#ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
+#rm -rf /apps/static/cdn/js/${VERSION} &&
+#cp -r /apps/static/cdn/releases/${VERSION}/js /apps/static/cdn/js/${VERSION} &&
+#rm -rf /apps/static/cdn/css/${VERSION} &&
+#cp -r /apps/static/cdn/releases/${VERSION}/css /apps/static/cdn/css/${VERSION} &&
+#rm -rf /apps/static/cdn/themes/${VERSION} &&
+#cp -r /apps/static/cdn/releases/${VERSION}/themes /apps/static/cdn/themes/${VERSION} &&
+#mkdir -p /apps/static/cdn/schemas/${VERSION} &&
+#cp /apps/static/cdn/releases/${VERSION}/json-schema.json /apps/static/cdn/schemas/${VERSION}/json-schema.json &&
+#cp /apps/static/cdn/releases/${VERSION}/xml-schema.xsd /apps/static/cdn/schemas/${VERSION}/xml-schema.xsd"
+#
+## copy DEV legacy files by version
+#if [ "${TRAVIS_BRANCH}" != "master" ]; then
+#    ssh -i ~/.ssh/id_rsa $STATIC_HOST_SSH_STRING "
+#    rm -rf /apps/static/js/${VERSION} &&
+#    cp -r /apps/static/cdn/releases/${VERSION}/js /apps/static/js/${VERSION} &&
+#    cp /apps/static/cdn/releases/${VERSION}/commit-hash.txt /apps/static/js/${VERSION}/commit-hash.txt &&
+#    rm -rf /apps/static/css/${VERSION} &&
+#    cp -r /apps/static/cdn/releases/${VERSION}/css /apps/static/css/${VERSION}"
+#fi
+#
+## copy CAT legacy files by branch name
+#if [ "${VERSION}" != "${TRAVIS_BRANCH}" ]; then
+#    ssh -i ~/.ssh/id_rsa  $STATIC_HOST_SSH_STRING "
+#    rm -rf /apps/static/cdn/releases/${TRAVIS_BRANCH} &&
+#    cp -r /apps/static/cdn/releases/${VERSION} /apps/static/cdn/releases/${TRAVIS_BRANCH}"
+#fi
+#
+## drop cdn cache for uploaded files
+#echo Dropping CDN cache
+#cd ../
+#python ./bin/drop_cdn_cache.py ${VERSION} ${CDN_ALIASE} ${CDN_CONSUMER_KEY} ${CDN_CONSUMER_SECRET} ${CDN_ZONE_ID}
 
 
 
