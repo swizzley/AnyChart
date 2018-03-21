f7c459e2e26743afb296e3e38c61295b56e1bfd4
Anton Saukh <grasshoppergn@gmail.com>
2018-03-21 11:31:41 +0800

    DateTimeScale fix

--- src/scales/DateTimeTicks.js
+++ src/scales/DateTimeTicks.js
@@ -398,14 +398,17 @@ anychart.scales.DateTimeTicks.prototype.setupAsMajor = function(min, max, opt_ca
     }
     if (!interval)
       interval = this.calculateIntervals_(min, max, false);
-    if (opt_canModifyMin)
-      result[0] = min = anychart.utils.alignDateLeft(min, interval, 0);
+    min = anychart.utils.alignDateLeft(min, interval, 0);
+    if (opt_canModifyMin) {
+      result[0] = min;
+    }
     var date = new goog.date.UtcDateTime(new Date(min));
     var endDate = new goog.date.UtcDateTime(new Date(max));
     for (var i = 0; goog.date.Date.compare(date, endDate) <= 0 && i < this.scale.maxTicksCount(); date.add(interval), i++)
       ticks.push(date.getTime());
-    if (opt_canModifyMax && ticks.length && ticks[ticks.length - 1] < endDate.getTime())
-      ticks.push(result[1] = date.getTime());
+    if (opt_canModifyMax && (!ticks.length || ticks[ticks.length - 1] < endDate.getTime())) {
+      result[1] = date.getTime();
+    }
     this.autoTicks_ = ticks;
     this.count_ = backupCount;
     this.intervalValue_ = interval;
