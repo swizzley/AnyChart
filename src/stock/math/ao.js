04f06d88d10e50059dc69b32755695f7dcaa0feb
Sergey Baranov <baranovs-v@mail.ru>
2018-04-20 17:41:58 +0800

    init commit

---
+++ src/stock/math/ao.js
@@ -0,0 +1,4 @@
+goog.provide('anychart.stockModule.math.ao');
+goog.require('anychart.stockModule.math.CycledQueue');
+goog.require('anychart.stockModule.math.sma');
+goog.require('anychart.utils');
