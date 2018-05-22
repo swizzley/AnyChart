d400b395d1eeb5a6fd3b79a2c2a70c8fed6c8097
blackart <blackart.ru@gmail.com>
2018-05-22 18:01:36 +0800

    w

--- src/core/drawers/Column.js
+++ src/core/drawers/Column.js
@@ -67,6 +67,7 @@ anychart.core.drawers.Column.prototype.requiredShapes = (function() {
 /** @inheritDoc */
 anychart.core.drawers.Column.prototype.drawSubsequentPoint = function(point, state) {
   var shapes = /** @type {Object.<acgraph.vector.Path>} */(this.shapesManager.getShapesGroup(state));
+
   this.drawPointShape(point, shapes['path'], shapes['hatchFill']);
 };
 
